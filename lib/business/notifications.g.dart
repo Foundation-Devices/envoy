ec20d3bb66a8c0e6a77da14911ce48e23652466b
Igor Cota <igor@openbook.hr>
2022-07-27 12:46:05 +0200

    Regenerate code

--- lib/business/notifications.g.dart
+++ lib/business/notifications.g.dart
@@ -1,7 +1,3 @@
-// SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'notifications.dart';
---
+++ lib/business/scv_server.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of envoy.scv_server;
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Challenge _$ChallengeFromJson(Map<String, dynamic> json) => Challenge(
+      json['challenge'] as String,
+      json['signature'] as String,
+      json['derSignature'] as String,
+    );
+
+Map<String, dynamic> _$ChallengeToJson(Challenge instance) => <String, dynamic>{
+      'challenge': instance.id,
+      'signature': instance.signature,
+      'derSignature': instance.derSignature,
+    };
