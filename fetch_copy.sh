14cfe2d11b462f80683f68ddd2da532a0fd5d887
Igor Cota <igor@openbook.hr>
2022-09-20 12:16:50 +0200

    ditto: update script, fetch latest copy

--- fetch_copy.sh
+++ fetch_copy.sh
@@ -16,6 +16,7 @@ ditto-cli pull
 
 # Clean and merge .jsons, output to l10n folder
 cd ditto
-jq 'with_entries(.key = "component_" + .key)' ditto-component-library.json > components.json
-jq -s '.[0] * .[1] * .[2]' components.json 'Envoy + PP Clean Start.json' Envoy.json | \
+jq 'with_entries(.key = "component_" + .key)' ditto-component-library__base.json > components.json
+jq 'with_entries(.key = .key + "_after_onboarding")' 'Envoy + PP Clean Start__after-onboarding.json' > after-onboarding.json
+jq -s '.[0] * .[1] * .[2] * .[3]' components.json 'Envoy + PP Clean Start__base.json' Envoy__base.json after-onboarding.json | \
 jq 'with_entries(.value = (.value | sub("\\\\n"; "\n")))' > ../lib/l10n/intl_en.arb
