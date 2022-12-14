30c9977e02b43ac2337ef2c0bad31b39104c450c
invertedx <invertedx@foundationdevices.com>
2022-12-14 23:04:34 +0530

    rm un-used import

--- lib/ui/home/cards/text_entry.dart
+++ lib/ui/home/cards/text_entry.dart
@@ -2,7 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import 'package:flutter/services.dart';
 import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 
 class TextEntry extends StatelessWidget {
