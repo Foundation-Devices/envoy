6b3a918d80ab75aa3b958c880c48509039a2025f
Igor Cota <igor@openbook.hr>
2023-02-16 14:38:39 +0100

    Bump NDK to 25.1.8937393

--- packages/backup/native/backup-ffi/build.rs
+++ packages/backup/native/backup-ffi/build.rs
@@ -25,7 +25,7 @@ fn main() {
         .unwrap()
         .write_to_file(&output_file);
 
-    // println!("cargo:rustc-link-search=native=/home/igor/Android/Sdk/ndk/22.1.7171670/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android");
+    // println!("cargo:rustc-link-search=native=/home/igor/Android/Sdk/ndk/25.1.8937393/toolchains/llvm/prebuilt/linux-x86_64/sysroot/usr/lib/aarch64-linux-android");
     // println!("cargo:rustc-link-lib=static=stdc++");
     //println!("cargo:rustc-link-lib=dylib=stdc++");
 }
