c860699b7623024bc15d018e701062da5d1a7172
invertedx <invertedx@foundationdevices.com>
2023-04-27 20:35:02 +0530

    new ui assets and missing licence header

--- lib/ui/state/onboarding_state.dart
+++ lib/ui/state/onboarding_state.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import 'package:envoy/business/connectivity_manager.dart';
 import 'package:envoy/business/settings.dart';
 import 'package:envoy/util/haptics.dart';
