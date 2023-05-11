6d31190d1d899e2f7ab8a6d82ffbf2a554a6e1e4
Igor Cota <igor@openbook.hr>
2023-05-11 22:48:13 +0200

    Add embedded_video and magic backups short film

--- lib/ui/onboard/magic/magic_setup_tutorial.dart
+++ lib/ui/onboard/magic/magic_setup_tutorial.dart
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import 'package:envoy/ui/embedded_video.dart';
 import 'package:envoy/ui/envoy_button.dart';
 import 'package:envoy/ui/envoy_colors.dart';
 import 'package:envoy/ui/onboard/magic/magic_recover_wallet.dart';
@@ -23,6 +24,8 @@ class MagicSetupTutorial extends StatefulWidget {
 }
 
 class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
+  GlobalKey<EmbeddedVideoState> _playerKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return OnboardPageBackground(
@@ -61,8 +64,13 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
                     child: Container(
                   padding: const EdgeInsets.all(10.0),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
+                      EmbeddedVideo(
+                        path: "assets/videos/magic_backups.mp4",
+                        key: _playerKey,
+                      ),
+                      Padding(padding: EdgeInsets.all(16)),
                       Text(
                         S().magic_setup_tutorial_ios_heading,
                         textAlign: TextAlign.center,
@@ -74,6 +82,7 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
                             ? S().magic_setup_tutorial_android_subheading
                             : S().magic_setup_tutorial_ios_subheading,
                         onTap: () {
+                          _playerKey.currentState?.pause();
                           showEnvoyDialog(
                               context: context,
                               dialog: WalletSecurityModal(
@@ -86,33 +95,26 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
                     ],
                   ),
                 )),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox.shrink(),
-                ),
-                Flexible(
-                    child: Column(
+                Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
-                    Container(
-                      width: double.infinity,
-                      child: OnboardingButton(
-                          fontWeight: FontWeight.w600,
-                          type: EnvoyButtonTypes.secondary,
-                          label: S().magic_setup_tutorial_ios_CTA2,
-                          textStyle: Theme.of(context)
-                              .textTheme
-                              .bodyMedium
-                              ?.copyWith(
-                                  color: EnvoyColors.darkTeal,
-                                  fontWeight: FontWeight.w600),
-                          onTap: () {
-                            Navigator.of(context)
-                                .push(MaterialPageRoute(builder: (context) {
-                              return MagicRecoverWallet();
-                            }));
-                          }),
-                    ),
+                    OnboardingButton(
+                        fontWeight: FontWeight.w600,
+                        type: EnvoyButtonTypes.secondary,
+                        label: S().magic_setup_tutorial_ios_CTA2,
+                        textStyle: Theme.of(context)
+                            .textTheme
+                            .bodyMedium
+                            ?.copyWith(
+                                color: EnvoyColors.darkTeal,
+                                fontWeight: FontWeight.w600),
+                        onTap: () {
+                          _playerKey.currentState?.pause();
+                          Navigator.of(context)
+                              .push(MaterialPageRoute(builder: (context) {
+                            return MagicRecoverWallet();
+                          }));
+                        }),
                     OnboardingButton(
                         fontWeight: FontWeight.w600,
                         textStyle: Theme.of(context)
@@ -123,6 +125,7 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
                                 fontWeight: FontWeight.w600),
                         label: S().magic_setup_tutorial_ios_CTA1,
                         onTap: () {
+                          _playerKey.currentState?.pause();
                           Navigator.of(context)
                               .push(MaterialPageRoute(builder: (context) {
                             return MagicSetupGenerate();
@@ -130,7 +133,7 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
                           // showCreateWarning(context);
                         }),
                   ],
-                ))
+                )
               ],
             ),
           ),
@@ -139,56 +142,56 @@ class _MagicSetupTutorialState extends State<MagicSetupTutorial> {
     ));
   }
 
-  // void showCreateWarning(BuildContext context) {
-  //   showEnvoyDialog(
-  //     context: context,
-  //     dismissible: true,
-  //     builder: Builder(builder: (context) {
-  //       return Container(
-  //         width: MediaQuery.of(context).size.width * 0.8,
-  //         child: Padding(
-  //           padding: const EdgeInsets.symmetric(horizontal: 24),
-  //           child: Column(
-  //             crossAxisAlignment: CrossAxisAlignment.center,
-  //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  //             mainAxisSize: MainAxisSize.min,
-  //             children: [
-  //               Padding(padding: EdgeInsets.all(24)),
-  //               Column(
-  //                 children: [
-  //                   Icon(EnvoyIcons.exclamation_warning,
-  //                       color: EnvoyColors.darkCopper, size: 56),
-  //                   Padding(padding: EdgeInsets.all(12)),
-  //                   Padding(
-  //                     padding: const EdgeInsets.symmetric(vertical: 8),
-  //                     child: Text(
-  //                       Platform.isAndroid
-  //                           ? S()
-  //                               .magic_setup_generate_wallet_modal_android_subheading
-  //                           : S()
-  //                               .magic_setup_generate_wallet_modal_ios_subheading,
-  //                       textAlign: TextAlign.center,
-  //                     ),
-  //                   ),
-  //                 ],
-  //               ),
-  //               OnboardingButton(
-  //                   label: Platform.isAndroid
-  //                       ? S().magic_setup_generate_wallet_modal_android_CTA
-  //                       : S().magic_setup_generate_wallet_modal_ios_CTA,
-  //                   onTap: () async {
-  //                     Navigator.pop(context);
-  //                     Navigator.of(context)
-  //                         .push(MaterialPageRoute(builder: (context) {
-  //                       return MagicSetupGenerate();
-  //                     }));
-  //                   }),
-  //               Padding(padding: EdgeInsets.all(12)),
-  //             ],
-  //           ),
-  //         ),
-  //       );
-  //     }),
-  //   );
-  // }
+// void showCreateWarning(BuildContext context) {
+//   showEnvoyDialog(
+//     context: context,
+//     dismissible: true,
+//     builder: Builder(builder: (context) {
+//       return Container(
+//         width: MediaQuery.of(context).size.width * 0.8,
+//         child: Padding(
+//           padding: const EdgeInsets.symmetric(horizontal: 24),
+//           child: Column(
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               Padding(padding: EdgeInsets.all(24)),
+//               Column(
+//                 children: [
+//                   Icon(EnvoyIcons.exclamation_warning,
+//                       color: EnvoyColors.darkCopper, size: 56),
+//                   Padding(padding: EdgeInsets.all(12)),
+//                   Padding(
+//                     padding: const EdgeInsets.symmetric(vertical: 8),
+//                     child: Text(
+//                       Platform.isAndroid
+//                           ? S()
+//                               .magic_setup_generate_wallet_modal_android_subheading
+//                           : S()
+//                               .magic_setup_generate_wallet_modal_ios_subheading,
+//                       textAlign: TextAlign.center,
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//               OnboardingButton(
+//                   label: Platform.isAndroid
+//                       ? S().magic_setup_generate_wallet_modal_android_CTA
+//                       : S().magic_setup_generate_wallet_modal_ios_CTA,
+//                   onTap: () async {
+//                     Navigator.pop(context);
+//                     Navigator.of(context)
+//                         .push(MaterialPageRoute(builder: (context) {
+//                       return MagicSetupGenerate();
+//                     }));
+//                   }),
+//               Padding(padding: EdgeInsets.all(12)),
+//             ],
+//           ),
+//         ),
+//       );
+//     }),
+//   );
+// }
 }
