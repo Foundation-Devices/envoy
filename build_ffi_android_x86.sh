6ed13385ed6c5b75d2cdeec3430105ccc14b1781
Igor Cota <igor@openbook.hr>
2023-05-20 10:15:25 +0200

    android: add RANLIB var to build script

--- build_ffi_android_x86.sh
+++ build_ffi_android_x86.sh
@@ -13,5 +13,6 @@ rustup target add x86_64-linux-android
 # Required for Cargo to build libtor-sys
 export PATH=$PATH:$ANDROID_SDK_ROOT/ndk/24.0.8215888/toolchains/llvm/prebuilt/linux-x86_64/bin/
 export AR=llvm-ar
+export RANLIB=llvm-ranlib
 
 cargo build --target=x86_64-linux-android
