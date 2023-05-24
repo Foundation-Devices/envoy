875b1b65c2fe4b3e4acacb9585a2c4c1e5dd0e4e
invertedx <invertedx@foundationdevices.com>
2023-05-24 23:22:07 +0530

    log reporting

--- lib/ui/home/settings/logs_report.dart
+++ lib/ui/home/settings/logs_report.dart
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import 'dart:math';
-
 import 'package:envoy/ui/envoy_colors.dart';
 import 'package:envoy/util/bug_report_helper.dart';
 import 'package:flutter/material.dart';
