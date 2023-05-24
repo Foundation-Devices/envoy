78ac30a7942f56d654c9b47a3cf034d38d4e2eb3
Igor Cota <igor@openbook.hr>
2023-05-24 08:00:20 +0200

    ios: remove dylib files after Rust build

--- build_ffi_ios.sh
+++ build_ffi_ios.sh
@@ -17,3 +17,10 @@ cargo +1.69.0 build --target=aarch64-apple-ios --release
 # (This'll work when https://github.com/alexcrichton/openssl-src-rs/pull/131 is merged/released)
 rustup +1.69.0 target add aarch64-apple-ios-sim
 cargo +1.69.0 build --target=aarch64-apple-ios-sim
+
+# Workaround to not being able to specify --crate-type=staticlib on a virtual package
+# Having the dynamic libraries in these folders results in a runtime error
+rm target/aarch64-apple-ios/debug/*.dylib
+rm target/aarch64-apple-ios/release/*.dylib
+rm target/aarch64-apple-ios-sim/debug/*.dylib
+
