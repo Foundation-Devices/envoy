19a1d3588e1a30451660069d48e099e2fc8ad9ba
Igor Cota <igor@openbook.hr>
2023-05-26 14:51:25 +0200

    SFT-1739: implement fancier scanner button

--- lib/ui/shield.dart
+++ lib/ui/shield.dart
@@ -38,3 +38,31 @@ class Shield extends StatelessWidget {
     );
   }
 }
+
+class QrShield extends StatelessWidget {
+  const QrShield({
+    Key? key,
+    required this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Neumorphic(
+      style: NeumorphicStyle(
+        boxShape: NeumorphicBoxShape.path(ShieldPath()),
+        shape: NeumorphicShape.flat,
+        border: NeumorphicBorder(
+          color: Colors.white,
+          width: 1,
+        ),
+        color: EnvoyColors.white01,
+        depth: 5,
+        lightSource: LightSource.top,
+        intensity: 1,
+      ),
+      child: Container(child: child),
+    );
+  }
+}
