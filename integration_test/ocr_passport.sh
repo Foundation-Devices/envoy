d17056b295091d48a2c8135c9c31f55ef91ac5cc
Igor Cota <igor@openbook.hr>
2023-07-16 18:54:15 +0200

    integration_test: add missing copyright headers

--- integration_test/ocr_passport.sh
+++ integration_test/ocr_passport.sh
@@ -1,1 +1,5 @@
+# SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 v4l2-ctl --device $1 --stream-mmap --stream-to=- --stream-count=1 | ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt rgb565 -s 240x320 -i - -f image2 -vcodec png -vframes 1 - | tesseract stdin stdout
