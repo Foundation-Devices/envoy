223d165731d03a24ae99c2ac9f7524f888e25279
invertedx <invertedx@foundationdevices.com>
2023-08-04 13:12:36 +0530

    [WIP] add coin sort options

--- lib/ui/home/cards/accounts/detail/coins/coins_state.dart
+++ lib/ui/home/cards/accounts/detail/coins/coins_state.dart
@@ -1,5 +1,10 @@
+// SPDX-FileCopyrightText: 2023 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import 'package:envoy/business/coin_tag.dart';
 import 'package:envoy/business/coins.dart';
+import 'package:envoy/ui/home/cards/accounts/detail/filter_state.dart';
 import 'package:envoy/ui/state/accounts_state.dart';
 import 'package:envoy/ui/storage/coins_repository.dart';
 import 'package:envoy/util/list_utils.dart';
@@ -22,16 +27,39 @@ class CoinStateNotifier extends StateNotifier<Set<String>> {
     state = newState;
   }
 
+  addAll(List<String> coinIds) {
+    //Creates new state from existing state,
+    //this is required for state notifier to notify the state change
+    final newState = {...state}..addAll(coinIds);
+    state = newState;
+  }
+
   remove(String coinId) {
     if (state.contains(coinId)) {
       final newState = {...state}..remove(coinId);
       state = newState;
     }
   }
+
+  void removeAll(List<String> list) {
+    bool changed = false;
+    Set<String> newState = {...state};
+    list.forEach((element) {
+      if (state.contains(element)) {
+        changed = true;
+        newState.remove(element);
+      }
+    });
+    if (changed) state = newState;
+  }
+
+  void reset() {
+    state = {};
+  }
 }
 
 final coinSelectionStateProvider =
-    StateNotifierProvider<CoinStateNotifier, Set>(
+    StateNotifierProvider<CoinStateNotifier, Set<String>>(
   (ref) => CoinStateNotifier(Set()),
 );
 
@@ -68,7 +96,12 @@ final coinsProvider = Provider.family<List<Coin>, String>((ref, accountId) {
 final coinsTagProvider =
     Provider.family<List<CoinTag>, String>((ref, accountId) {
   final existingTags = ref.watch(coinTagsStreamProvider(accountId)).value ?? [];
-  final coins = ref.watch(coinsProvider(accountId));
+  final filter = ref.watch(coinFilterStateProvider);
+  final existingCoins = ref.watch(coinsProvider(accountId));
+  final utxoFilterState = ref.watch(coinFilterStateProvider);
+  //Make deep copies so it wont affect existing data sets, any array operations like
+  //removeWhere or Add needs to be done in deep copies.
+  List<Coin> coins = [...existingCoins];
   List<CoinTag> tags = [...existingTags];
   //Map coins to tags
   tags.forEach((tag) {
@@ -78,7 +111,7 @@ final coinsTagProvider =
     tag.coins = coinsAssociated;
     coins.removeWhere((element) => tag.coins_id.contains(element.id));
   });
-
+  //add coins that are not associated with any tag
   if (coins.isNotEmpty)
     tags.add(CoinTag(
         id: CoinTag.generateNewId(),
@@ -86,6 +119,36 @@ final coinsTagProvider =
         account: accountId,
         untagged: true)
       ..addCoins(coins));
-  //add coins that are not associated with any tag
+
+  //sort tags based on Tag name
+  if (filter.filterByTagName != FilterButtonState.none) {
+    tags.sort((a, b) => filter.filterByTagName == FilterButtonState.up
+        ? a.name.compareTo(b.name)
+        : b.name.compareTo(a.name));
+  }
+
+  //sort tags based on the number of coins in each tags
+  if (utxoFilterState.filterByNumberOfCoins != FilterButtonState.none) {
+    tags.sort((a, b) {
+      if (utxoFilterState.filterByNumberOfCoins == FilterButtonState.down) {
+        return a.coins.length.compareTo(b.coins.length);
+      } else {
+        return b.coins.length.compareTo(a.coins.length);
+      }
+    });
+  }
+  //sort tags based on the total amount contained in each tags
+  if (utxoFilterState.filterByAmount != FilterButtonState.none) {
+    tags.sort(
+      (a, b) {
+        if (utxoFilterState.filterByAmount == FilterButtonState.down) {
+          return a.totalAmount.compareTo(b.totalAmount);
+        } else {
+          return b.totalAmount.compareTo(a.totalAmount);
+        }
+      },
+    );
+  }
+
   return tags;
 });
