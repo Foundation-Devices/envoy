fa811497e1c0c3ddc522b51acbe7b54f0044c545
invertedx <invertedx@foundationdevices.com>
2023-08-04 14:02:38 +0530

    [WIP] fix licence

--- lib/util/tag_details_container_transform.dart
+++ lib/util/tag_details_container_transform.dart
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2023 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
