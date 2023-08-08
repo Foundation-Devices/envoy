f41962358a06c27bdfa639eaee7418287b7770f6
Igor Cota <igor@openbook.hr>
2023-08-08 13:19:56 +0200

    BEEFbench: run in Docker (and GitHub Actions)

--- integration_test_headless.sh
+++ integration_test_headless.sh
@@ -2,5 +2,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+#!/usr/bin/env bash
+
 Xvfb :5 -screen 0 1920x1080x24+32 -fbdir /var/tmp &
-DISPLAY=:5 flutter test integration_test -d linux
+DISPLAY=:5 flutter test integration_test -d linux -r github
+mkdir -p release
+cp screenshot.png release
