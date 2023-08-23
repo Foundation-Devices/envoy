1fd9060c9676d22fa79b58a12c976c2b90814fc1
Igor Cota <igor@openbook.hr>
2023-08-23 15:46:23 +0200

    SFT-2407: implement privacy sheet on home page

--- lib/ui/components/select_dropdown.dart
+++ lib/ui/components/select_dropdown.dart
@@ -8,50 +8,65 @@ import 'package:envoy/ui/theme/envoy_spacing.dart';
 import 'package:envoy/ui/theme/envoy_typography.dart';
 import 'package:flutter/material.dart';
 
-class SelectDropdown extends StatefulWidget {
-  final String labelOption1;
-  final String labelOption2;
-  final bool isDropdownActive;
+enum EnvoyDropdownOptionType { normal, personalNode }
 
-  SelectDropdown({
-    required this.labelOption1,
-    required this.labelOption2,
+class EnvoyDropdownOption {
+  final String label;
+  final EnvoyDropdownOptionType type;
+
+  EnvoyDropdownOption(this.label, {this.type = EnvoyDropdownOptionType.normal});
+}
+
+class EnvoyDropdown extends StatefulWidget {
+  final List<EnvoyDropdownOption> options;
+  final bool isDropdownActive;
+  final Function(EnvoyDropdownOption?)? onOptionChanged;
+  final int initialIndex;
+
+  EnvoyDropdown({
+    super.key,
+    required this.options,
     this.isDropdownActive = true,
+    this.onOptionChanged,
+    this.initialIndex = 0,
   });
 
   @override
-  _SelectDropdownState createState() => _SelectDropdownState();
+  _EnvoyDropdownState createState() => _EnvoyDropdownState();
 }
 
-class _SelectDropdownState extends State<SelectDropdown> {
-  String? selectedValue;
-  bool isTapped = true;
-  late FocusNode focusNode;
+class _EnvoyDropdownState extends State<EnvoyDropdown> {
+  int _selectedIndex = 0;
+  EnvoyDropdownOption? _selectedOption;
+  bool _isTapped = true;
+  late FocusNode _focusNode;
 
-  _SelectDropdownState() {
-    focusNode = FocusNode();
+  _EnvoyDropdownState() {
+    _focusNode = FocusNode();
   }
 
   @override
   void initState() {
     super.initState();
-    selectedValue = widget.labelOption1; // default
-    focusNode = FocusNode();
+    _selectedIndex = widget.initialIndex;
+    _selectedOption =
+        widget.options.isNotEmpty ? widget.options[_selectedIndex] : null;
+    _focusNode = FocusNode();
   }
 
   @override
   void dispose() {
-    focusNode.dispose();
+    _focusNode.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Focus(
-      focusNode: focusNode,
+      focusNode: _focusNode,
       onFocusChange: (hasFocus) {
         setState(() {
-          isTapped = hasFocus;
+          _isTapped = hasFocus;
         });
       },
       child: Container(
@@ -59,82 +74,93 @@ class _SelectDropdownState extends State<SelectDropdown> {
           color: widget.isDropdownActive
               ? EnvoyColors.surface2
               : EnvoyColors.surface2.withOpacity(0.5),
-          borderRadius: BorderRadius.circular(EnvoySpacing.xs),
+          borderRadius: BorderRadius.circular(EnvoySpacing.small),
           border: Border.all(
-            color: (!isTapped && widget.isDropdownActive)
+            color: (!_isTapped && widget.isDropdownActive)
                 ? EnvoyColors.accentPrimary
                 : Colors.transparent,
             width: 1,
           ),
         ),
         child: Padding(
-          padding: EdgeInsets.all(EnvoySpacing.medium1),
-          child: Theme(
-            data: ThemeData.light().copyWith(
-              focusColor: EnvoyColors.accentPrimary,
-            ),
-            child: DropdownButton<String>(
-              onChanged: widget.isDropdownActive
-                  ? (String? newValue) {
-                      setState(() {
-                        selectedValue = newValue;
-                      });
-                    }
-                  : null,
-              selectedItemBuilder: (BuildContext context) {
-                return <String>[widget.labelOption1, widget.labelOption2]
-                    .map((String value) {
-                  return Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      value,
-                      style: EnvoyTypography.body1Medium.copyWith(
-                        color: EnvoyColors.textPrimary,
-                      ),
+          padding: EdgeInsets.all(EnvoySpacing.small),
+          child: DropdownButton<EnvoyDropdownOption>(
+            elevation: EnvoySpacing.xs.toInt(),
+            borderRadius: BorderRadius.circular(EnvoySpacing.small),
+            onChanged: widget.isDropdownActive
+                ? (EnvoyDropdownOption? newValue) {
+                    setState(() {
+                      _selectedOption = newValue;
+                      _selectedIndex = widget.options.indexOf(newValue!);
+                      if (widget.onOptionChanged != null) {
+                        widget.onOptionChanged!(newValue);
+                      }
+                    });
+                  }
+                : null,
+            selectedItemBuilder: (BuildContext context) {
+              return widget.options.map((EnvoyDropdownOption option) {
+                return Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    option.label,
+                    style: EnvoyTypography.body1Medium.copyWith(
+                      color: EnvoyColors.textPrimary,
                     ),
-                  );
-                }).toList();
-              },
-              underline: Container(height: 0, width: 0),
-              value: selectedValue,
-              isExpanded: true,
-              items: <String>[widget.labelOption1, widget.labelOption2]
-                  .map<DropdownMenuItem<String>>((String value) {
-                final bool isSelectedLabel = (value == selectedValue);
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        value,
-                        style: EnvoyTypography.body1Medium.copyWith(
-                          color: isSelectedLabel && isTapped
-                              ? EnvoyColors.textPrimaryInverse
-                              : EnvoyColors.textPrimary,
-                        ),
-                      ),
-                      if (isTapped)
-                        Padding(
-                          padding:
-                              const EdgeInsets.only(left: EnvoySpacing.medium1),
-                          child: EnvoyIcon(
-                            EnvoyIcons.check,
-                            color: isSelectedLabel
-                                ? EnvoyColors.textPrimaryInverse
-                                : Colors.transparent,
-                          ),
-                        ),
-                    ],
                   ),
                 );
-              }).toList(),
-              icon: Padding(
-                padding: const EdgeInsets.only(left: EnvoySpacing.medium1),
-                child: EnvoyIcon(
-                  EnvoyIcons.chevron_down,
-                  color: EnvoyColors.textPrimary,
-                ),
+              }).toList();
+            },
+            underline: Container(height: 0, width: 0),
+            value: widget.options[_selectedIndex],
+            isExpanded: true,
+            items: widget.options.map<DropdownMenuItem<EnvoyDropdownOption>>(
+              (EnvoyDropdownOption option) {
+                final bool isSelectedOption =
+                    option.label == _selectedOption!.label;
+                return DropdownMenuItem<EnvoyDropdownOption>(
+                  value: option,
+                  child: Container(
+                    color: isSelectedOption
+                        ? EnvoyColors.accentPrimary
+                        : Colors.transparent,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14.0),
+                      //size of the blue background
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            option.label,
+                            style: EnvoyTypography.body1Medium.copyWith(
+                              color: isSelectedOption && _isTapped
+                                  ? EnvoyColors.textPrimaryInverse
+                                  : EnvoyColors.textPrimary,
+                            ),
+                          ),
+                          if (_isTapped)
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: EnvoySpacing.medium1),
+                              child: EnvoyIcon(
+                                EnvoyIcons.check,
+                                color: isSelectedOption
+                                    ? EnvoyColors.textPrimaryInverse
+                                    : Colors.transparent,
+                              ),
+                            ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ).toList(),
+            icon: Padding(
+              padding: const EdgeInsets.only(left: EnvoySpacing.medium1),
+              child: EnvoyIcon(
+                EnvoyIcons.chevron_down,
+                color: EnvoyColors.textPrimary,
               ),
             ),
           ),
--- lib/ui/home/cards/privacy/privacy_card.dart
+++ lib/ui/home/cards/privacy/privacy_card.dart
@@ -2,10 +2,26 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import 'package:envoy/business/connectivity_manager.dart';
 import 'package:envoy/ui/home/cards/tl_navigation_card.dart';
+import 'package:envoy/ui/theme/envoy_colors.dart';
+import 'package:envoy/ui/theme/envoy_spacing.dart';
+import 'package:envoy/ui/theme/envoy_typography.dart';
 import 'package:flutter/material.dart';
 import 'package:envoy/generated/l10n.dart';
 import 'package:envoy/ui/home/cards/navigation_card.dart';
+import 'package:envoy/ui/components/settings_header.dart';
+import 'package:envoy/ui/theme/envoy_icons.dart';
+import 'package:envoy/ui/components/big_tab.dart';
+import 'package:envoy/ui/onboard/onboarding_page.dart';
+import 'package:envoy/ui/components/select_dropdown.dart';
+import 'package:envoy/ui/components/toggle.dart';
+import 'package:envoy/business/settings.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:envoy/business/local_storage.dart';
+import 'package:envoy/ui/fading_edge_scroll_view.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:envoy/ui/home/settings/electrum_server_entry.dart';
 
 //ignore: must_be_immutable
 class PrivacyCard extends StatefulWidget with TopLevelNavigationCard {
@@ -30,7 +46,7 @@ class PrivacyCardState extends State<PrivacyCard>
 }
 
 //ignore: must_be_immutable
-class TopLevelPrivacyCard extends StatelessWidget with NavigationCard {
+class TopLevelPrivacyCard extends StatefulWidget with NavigationCard {
   TopLevelPrivacyCard() {}
 
   @override
@@ -55,9 +71,244 @@ class TopLevelPrivacyCard extends StatelessWidget with NavigationCard {
   String? title = S().bottomNav_privacy.toUpperCase();
 
   @override
+  _TopLevelPrivacyCardState createState() => _TopLevelPrivacyCardState();
+}
+
+//ignore: must_be_immutable
+class _TopLevelPrivacyCardState extends State<TopLevelPrivacyCard> {
+  bool _showPersonalNodeTextField = false;
+  bool _betterPerformance = Settings().torEnabled();
+
+  bool _useLocalAuth = false;
+  final ScrollController _scrollController = ScrollController();
+  final LocalAuthentication _auth = LocalAuthentication();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      bool? value = LocalStorage().prefs.getBool("useLocalAuth");
+      if (value != null)
+        setState(() {
+          _useLocalAuth = value;
+        });
+    });
+  }
+
+  void _handleDropdownChange(EnvoyDropdownOption newOption) {
+    setState(() {
+      _showPersonalNodeTextField =
+          newOption.type == EnvoyDropdownOptionType.personalNode;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
+    var s = Settings();
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: EnvoySpacing.large2),
+        child: FadingEdgeScrollView.fromSingleChildScrollView(
+            gradientFractionOnEnd: 0.2,
+            gradientFractionOnStart: 0.2,
+            child: SingleChildScrollView(
+              controller: _scrollController,
+              child: Container(
+                child: Padding(
+                  padding: const EdgeInsets.all(EnvoySpacing.medium1),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(top: EnvoySpacing.medium1),
+                        child: SettingsHeader(
+                          title: S().privacy_privacyMode_title,
+                          linkText: S().privacy_privacyMode_learnMore,
+                          onTap: () {
+                            launchUrl(
+                                Uri.parse(
+                                    "https://docs.foundationdevices.com/envoy/privacy"),
+                                mode: LaunchMode.externalApplication);
+                          },
+                          icon: EnvoyIcons.privacy,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: EnvoySpacing.medium2),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            BigTab(
+                              label: S().privacy_privacyMode_betterPerformance,
+                              icon: EnvoyIcons.performance,
+                              isActive: _betterPerformance,
+                              onSelect: (isActive) {
+                                setState(() {
+                                  _betterPerformance = true;
+                                });
+                                Settings().setTorEnabled(!_betterPerformance);
+                              },
+                            ),
+                            BigTab(
+                              label: S().privacy_privacyMode_improvedPrivacy,
+                              icon: EnvoyIcons.privacy,
+                              isActive: !_betterPerformance,
+                              onSelect: (isActive) {
+                                setState(() {
+                                  _betterPerformance = false;
+                                });
+                                Settings().setTorEnabled(!_betterPerformance);
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      LinkText(
+                          text: _betterPerformance == true
+                              ? S().privacy_privacyMode_torSuggestion
+                              : S().privacy_privacyMode_torSuggestionOn,
+                          textStyle: EnvoyTypography.caption1Medium
+                              .copyWith(color: EnvoyColors.textSecondary),
+                          linkStyle: EnvoyTypography.caption1Semibold.copyWith(
+                              color: _betterPerformance == true
+                                  ? EnvoyColors.accentSecondary
+                                  : EnvoyColors.accentPrimary)),
+                      buildDivider(),
+                      SettingsHeader(
+                        title: S().privacy_node_title,
+                        linkText: S().privacy_node_learnMore,
+                        onTap: () {
+                          launchUrl(
+                              Uri.parse(
+                                  "https://docs.foundationdevices.com/envoy/privacy"),
+                              mode: LaunchMode.externalApplication);
+                        },
+                        icon: EnvoyIcons.node,
+                      ),
+                      Padding(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: EnvoySpacing.medium2)),
+                      EnvoyDropdown(
+                        initialIndex:
+                            ConnectivityManager().usingDefaultServer ? 0 : 1,
+                        options: [
+                          EnvoyDropdownOption(
+                              S().privacy_node_nodeType_foundation),
+                          EnvoyDropdownOption(
+                              S().privacy_node_nodeType_personal,
+                              type: EnvoyDropdownOptionType.personalNode),
+                        ],
+                        onOptionChanged: (selectedOption) {
+                          if (selectedOption != null) {
+                            setState(() {
+                              _handleDropdownChange(selectedOption);
+                              if (!(selectedOption.type ==
+                                  EnvoyDropdownOptionType.personalNode))
+                                s.useDefaultElectrumServer(true);
+                            });
+                          }
+                        },
+                      ),
+                      if (!ConnectivityManager().usingDefaultServer ||
+                          _showPersonalNodeTextField)
+                        Padding(
+                          padding:
+                              const EdgeInsets.only(top: EnvoySpacing.medium1),
+                          child: SingleChildScrollView(
+                              child: ElectrumServerEntry(
+                                  s.customElectrumAddress,
+                                  s.setCustomElectrumAddress)),
+                        ),
+                      FutureBuilder<bool>(
+                        future: _auth.isDeviceSupported(),
+                        builder: (context, snapshot) {
+                          if (!snapshot.hasData) {
+                            return SizedBox();
+                          }
+                          if (snapshot.hasData && snapshot.data! == false) {
+                            return SizedBox();
+                          }
+
+                          return Column(
+                            children: [
+                              buildDivider(),
+                              Row(
+                                children: [
+                                  EnvoyIcon(EnvoyIcons.biometrics,
+                                      size: EnvoyIconSize.normal),
+                                  SizedBox(
+                                    width: EnvoySpacing.small,
+                                  ),
+                                  Text(
+                                    S().privacy_applicationLock_title,
+                                    style: EnvoyTypography.body1Medium.copyWith(
+                                        color: EnvoyColors.textPrimary),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: EnvoySpacing.medium2)),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    S().privacy_applicationLock_unlock,
+                                    style: EnvoyTypography.body2Medium.copyWith(
+                                      color: EnvoyColors.textPrimary,
+                                    ),
+                                  ),
+                                  EnvoyToggle(
+                                    value: _useLocalAuth,
+                                    onChanged: (bool value) async {
+                                      try {
+                                        bool authSuccess =
+                                            await _auth.authenticate(
+                                          options: AuthenticationOptions(
+                                            biometricOnly: false,
+                                          ),
+                                          localizedReason:
+                                              "Authenticate to Enable Biometrics",
+                                        );
+
+                                        if (authSuccess) {
+                                          LocalStorage()
+                                              .prefs
+                                              .setBool("useLocalAuth", value);
+                                          setState(() {
+                                            _useLocalAuth = value;
+                                          });
+                                        }
+                                      } catch (e) {
+                                        print(e);
+                                      }
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          );
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+
+  Padding buildDivider() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: EnvoySpacing.medium2),
+      child: Divider(
+        height: 2,
+        color: EnvoyColors.border1,
+      ),
     );
   }
 }
