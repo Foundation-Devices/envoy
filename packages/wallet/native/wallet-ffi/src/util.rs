53ccc23484837937562b1c6da188773293864668
Igor Cota <igor@openbook.hr>
2023-08-23 13:17:55 +0200

    util.rs: add licence header

--- packages/wallet/native/wallet-ffi/src/util.rs
+++ packages/wallet/native/wallet-ffi/src/util.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use crate::{
     serialize, Address, Client, OutPoint, PartiallySignedTransaction, Psbt, Socks5Config, Txid,
     UtxoList,
