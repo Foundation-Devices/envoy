d595e6f32aa62b773320e6c683a572af3886225c
Igor Cota <igor@openbook.hr>
2023-08-24 15:05:04 +0200

    SFT-2622: throw exception when Tor fails

--- packages/tor/native/tor-ffi/src/lib.rs
+++ packages/tor/native/tor-ffi/src/lib.rs
@@ -2,45 +2,68 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-//use std::ffi::CString;
 //use android_log_sys::__android_log_write;
 use arti::socks;
 use arti_client::config::TorClientConfigBuilder;
 use arti_client::TorClient;
 use lazy_static::lazy_static;
-use std::io;
+use std::ffi::{c_char, CString};
+use std::{io, ptr};
 use tokio::runtime::{Builder, Runtime};
 use tor_rtcompat::tokio::TokioNativeTlsRuntime;
 use tor_rtcompat::BlockOn;
+
+mod error;
 
 lazy_static! {
     static ref RUNTIME: io::Result<Runtime> = Builder::new_multi_thread().enable_all().build();
 }
 
 #[no_mangle]
+pub unsafe extern "C" fn tor_last_error_message() -> *const c_char {
+    let last_error = match error::take_last_error() {
+        Some(err) => err,
+        None => return CString::new("").unwrap().into_raw(),
+    };
+
+    let error_message = last_error.to_string();
+    CString::new(error_message).unwrap().into_raw()
+}
+
+macro_rules! unwrap_or_return {
+    ($a:expr,$b:expr) => {
+        match $a {
+            Ok(x) => x,
+            Err(e) => {
+                error::update_last_error(e);
+                return $b;
+            }
+        }
+    };
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn tor_start(socks_port: u16) -> *mut TorClient<TokioNativeTlsRuntime> {
-    let runtime = TokioNativeTlsRuntime::create().unwrap();
+    let err_ret = ptr::null_mut();
+    let runtime = unwrap_or_return!(TokioNativeTlsRuntime::create(), err_ret);
 
-    let state_dir = tempfile::tempdir().unwrap();
-    let cache_dir = tempfile::tempdir().unwrap();
-    let cfg = TorClientConfigBuilder::from_directories(state_dir, cache_dir)
-        .build()
-        .unwrap();
+    let state_dir = unwrap_or_return!(tempfile::tempdir(), err_ret);
+    let cache_dir = unwrap_or_return!(tempfile::tempdir(), err_ret);
 
-    // __android_log_write(
-    //     6,
-    //     CString::new("envoy").unwrap().into_raw(),
-    //     CString::new("BOOTSTRAP").unwrap().into_raw(),
-    // );
+    let cfg = unwrap_or_return!(
+        TorClientConfigBuilder::from_directories(state_dir, cache_dir).build(),
+        err_ret
+    );
 
-    let client = runtime.block_on(async {
-        let res = TorClient::with_runtime(runtime.clone())
-            .config(cfg)
-            .create_bootstrapped()
-            .await;
-
-        res.unwrap()
-    });
+    let client = unwrap_or_return!(
+        runtime.block_on(async {
+            TorClient::with_runtime(runtime.clone())
+                .config(cfg)
+                .create_bootstrapped()
+                .await
+        }),
+        err_ret
+    );
 
     let client_clone = client.clone();
 
@@ -66,7 +89,7 @@ pub unsafe extern "C" fn tor_bootstrap(client: *mut TorClient<TokioNativeTlsRunt
         &mut *client
     };
 
-    client.runtime().block_on(client.bootstrap()).unwrap();
+    unwrap_or_return!(client.runtime().block_on(client.bootstrap()), false);
     true
 }
 
