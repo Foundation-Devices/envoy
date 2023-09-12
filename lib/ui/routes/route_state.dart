bea9345b556b20f9a1a08968660dd4d40b5167e7
invertedx <invertedx@foundationdevices.com>
2023-09-12 19:37:53 +0530

    use go_router as main router

---
+++ lib/ui/routes/route_state.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+
+import 'package:envoy/ui/home/home_state.dart';
+import 'package:envoy/ui/routes/routes.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+StreamController<RouteMatchList> routeStreamController =
+    StreamController<RouteMatchList>.broadcast();
+
+void listenToRouteChanges() {
+  mainRouter.routerDelegate.addListener(() {
+    routeStreamController.sink
+        .add(mainRouter.routerDelegate.currentConfiguration);
+  });
+}
+
+final _routerStreamProvider =
+    StreamProvider((ref) => routeStreamController.stream);
+
+final routePathProvider = Provider<String>((ref) {
+  RouteMatchList paths = ref.watch(_routerStreamProvider).value ??
+      mainRouter.routerDelegate.currentConfiguration;
+  return paths.fullPath;
+});
