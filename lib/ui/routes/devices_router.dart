5f733f9f69dd2a6bb28b71156ce0fcfa384d1020
invertedx <invertedx@foundationdevices.com>
2023-09-13 22:22:58 +0530

    devices router

---
+++ lib/ui/routes/devices_router.dart
@@ -0,0 +1,31 @@
+import 'package:envoy/business/devices.dart';
+import 'package:envoy/ui/home/cards/devices/device_card.dart';
+import 'package:envoy/ui/home/cards/devices/devices_card.dart';
+import 'package:envoy/ui/routes/accounts_router.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+const ROUTE_DEVICES = '/devices';
+
+const _SUB_ROUTE_DEVICE_DETAIL = 'details';
+const ROUTE_DEVICE_DETAIL = '${ROUTE_DEVICES}/${_SUB_ROUTE_DEVICE_DETAIL}';
+
+final devicesRouter =
+    StatefulShellBranch(restorationScopeId: 'devicesScopeId', routes: [
+  GoRoute(
+      path: ROUTE_DEVICES,
+      pageBuilder: (context, state) => MaterialPage(child: DevicesCard()),
+      routes: [
+        GoRoute(
+            path: _SUB_ROUTE_DEVICE_DETAIL,
+            pageBuilder: (context, state) {
+              Device? device;
+              if (state.extra is Map) {
+                device = Device.fromJson(state.extra as Map<String, dynamic>);
+              } else {
+                device = state.extra as Device;
+              }
+              return wrapWithVerticalAxisAnimation(DeviceCard(device));
+            }),
+      ]),
+]);
