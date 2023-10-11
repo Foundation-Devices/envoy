d4edacf2843b8f24d77cb21e3accf7a2b79302b6
invertedx <invertedx@foundationdevices.com>
2023-10-12 00:09:46 +0530

    list extension to remove duplicates from a list

--- lib/util/list_utils.dart
+++ lib/util/list_utils.dart
@@ -6,3 +6,12 @@ extension IterableModifier<E> on Iterable<E> {
   E? firstWhereOrNull(bool Function(E) test) =>
       cast<E?>().firstWhere((v) => v != null && test(v), orElse: () => null);
 }
+
+extension Unique<E, Id> on List<E> {
+  List<E> unique([Id Function(E element)? id, bool inplace = true]) {
+    final ids = Set();
+    var list = inplace ? this : List<E>.from(this);
+    list.retainWhere((x) => ids.add(id != null ? id(x) : x as Id));
+    return list;
+  }
+}
