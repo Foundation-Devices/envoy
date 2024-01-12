9c2ecbc6e96f4c60c2fceb76478ea753beda799b
Igor Cota <igor@openbook.hr>
2024-01-12 08:55:00 +0100

    main: document the nofile_limit bump

--- lib/main.dart
+++ lib/main.dart
@@ -43,8 +43,12 @@ Future<void> main() async {
 }
 
 Future<void> initSingletons() async {
+  // This is notoriously low on iOS, causing 'too many open files errors'
   print("Process nofile_limit: " + getNofileLimit().toString());
-  print("Process nofile_limit set to: " + setNofileLimit(256).toString());
+
+  // Requesting a high number. The API will return the best we can get
+  // ~10k on iPhone 11 which is much better than the default 256
+  print("Process nofile_limit bumped to: " + setNofileLimit(16384).toString());
 
   await EnvoyStorage().init();
   await LocalStorage.init();
