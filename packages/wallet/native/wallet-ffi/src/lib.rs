9426c7719d67eec2e908d9cda31d7979b58e7b79
invertedx <invertedx@foundationdevices.com>
2024-02-14 23:56:02 +0100

    fix rust formating

--- packages/wallet/native/wallet-ffi/src/lib.rs
+++ packages/wallet/native/wallet-ffi/src/lib.rs
@@ -1014,14 +1014,9 @@ pub unsafe extern "C" fn wallet_get_max_bumped_fee_rate(
                         bdk::Error::InsufficientFunds { available, .. } => {
                             max_fee = available;
                             rounds += 1;
-                            if rounds > 2 {
-                                if max_fee > amount {
-                                    match max_fee.checked_sub(amount) {
-                                        Some(available_for_fee) => {
-                                            max_fee = available_for_fee;
-                                        }
-                                        _ => {}
-                                    }
+                            if rounds > 2 && max_fee > amount {
+                                if let Some(available_for_fee) = max_fee.checked_sub(amount) {
+                                    max_fee = available_for_fee;
                                 }
                             }
                         }
