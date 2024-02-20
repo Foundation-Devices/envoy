bef7a130ea457ea9a888ae16f9dab8a308d00d4c
invertedx <invertedx@foundationdevices.com>
2024-02-20 11:37:25 +0100

    fix un-used imports

--- lib/ui/home/cards/accounts/detail/transaction/transactions_details.dart
+++ lib/ui/home/cards/accounts/detail/transaction/transactions_details.dart
@@ -29,7 +29,6 @@ import 'package:envoy/util/easing.dart';
 import 'package:envoy/util/envoy_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
