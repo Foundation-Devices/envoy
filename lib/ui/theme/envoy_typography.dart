de7cfdd4968276b9d212b80ac9991b402fcba697
Darijo Perković <darijo@openbook.hr>
2024-03-01 14:35:02 +0100

    fix check fail

--- lib/ui/theme/envoy_typography.dart
+++ lib/ui/theme/envoy_typography.dart
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+import 'dart:ui';
 import 'package:envoy/ui/theme/envoy_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
