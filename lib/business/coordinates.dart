05567af33dc81c511b6648b8f902fb13763903df
Darijo Perković <darijo@openbook.hr>
2024-06-05 16:18:08 +0200

    Check: add copyright and licensing information

--- lib/business/coordinates.dart
+++ lib/business/coordinates.dart
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 import 'package:json_annotation/json_annotation.dart';
 
 part 'coordinates.g.dart';
