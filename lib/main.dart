053ea1d6099766a9cf8848704ae1ea68192c0e5b
Mislav Stipić <mixslav@gmail.com>
2024-06-18 15:32:58 +0200

    Turn off overscroll indicator

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ import 'package:envoy/business/local_storage.dart';
 import 'package:envoy/business/notifications.dart';
 import 'package:envoy/business/settings.dart';
 import 'package:envoy/business/updates_manager.dart';
-import 'package:envoy/ui/envoy_colors.dart';
+import 'package:envoy/ui/theme/envoy_colors.dart';
 import 'package:envoy/ui/lock/authenticate_page.dart';
 import 'package:envoy/ui/routes/route_state.dart';
 import 'package:envoy/ui/routes/routes.dart';
@@ -103,7 +103,7 @@ class EnvoyApp extends StatelessWidget {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge,
         overlays: [SystemUiOverlay.top]);
 
-    const envoyAccentColor = EnvoyColors.darkTeal;
+    const envoyAccentColor = EnvoyColors.accentPrimary;
     const envoyBaseColor = Colors.transparent;
     final envoyTextTheme =
         GoogleFonts.montserratTextTheme(Theme.of(context).textTheme);
@@ -137,7 +137,21 @@ class EnvoyApp extends StatelessWidget {
             scaffoldBackgroundColor: envoyBaseColor,
             useMaterial3: false),
         routerConfig: mainRouter,
+        scrollBehavior: CustomScrollBehavior(),
       ),
+    );
+  }
+}
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return GlowingOverscrollIndicator(
+      axisDirection: AxisDirection.right,
+      color: EnvoyColors.textPrimaryInverse,
+      showLeading: false,
+      child: child, // Turn off overscroll indicator
     );
   }
 }
