c67d8a18c9cf0e73ae55633d6a0fc430c801f314
Darijo Perković <darijo@openbook.hr>
2024-06-24 16:17:12 +0200

    Open choose account menu from stacked account tile

--- lib/ui/home/cards/buy_bitcoin_account_selection.dart
+++ lib/ui/home/cards/buy_bitcoin_account_selection.dart
@@ -114,6 +114,7 @@ class _SelectAccountState extends ConsumerState<SelectAccount> {
                       filteredAccounts: filteredAccounts,
                       onTap: (Account account) {
                         updateSelectedAccount(account);
+                        chooseAccount(context, filteredAccounts);
                       },
                     ),
                     const SizedBox(
@@ -128,18 +129,7 @@ class _SelectAccountState extends ConsumerState<SelectAccount> {
                           textAlign: TextAlign.center,
                         ),
                         onTap: () {
-                          Navigator.of(rootNavigator: true, context)
-                              .push(MaterialTransparentRoute(
-                            builder: (context) {
-                              return BackdropFilter(
-                                  filter:
-                                      ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                                  child: ChooseAccount(
-                                    accounts: filteredAccounts,
-                                    onSelectAccount: updateSelectedAccount,
-                                  ));
-                            },
-                          ));
+                          chooseAccount(context, filteredAccounts);
                         },
                       );
                     }),
@@ -247,6 +237,19 @@ class _SelectAccountState extends ConsumerState<SelectAccount> {
         ),
       );
     }
+  }
+
+  void chooseAccount(BuildContext context, List<Account> filteredAccounts) {
+    Navigator.of(rootNavigator: true, context).push(MaterialTransparentRoute(
+      builder: (context) {
+        return BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+            child: ChooseAccount(
+              accounts: filteredAccounts,
+              onSelectAccount: updateSelectedAccount,
+            ));
+      },
+    ));
   }
 }
 
