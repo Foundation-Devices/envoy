c912461cda3ff7642bd81bc2f02dd6fa074a6703
Darijo Perković <darijo@openbook.hr>
2024-06-27 13:04:49 +0200

    Buy: add loading spinner while getting address

---
+++ lib/ui/components/envoy_loader.dart
@@ -0,0 +1,24 @@
+// SPDX-FileCopyrightText: 2022 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import 'package:envoy/ui/theme/envoy_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:envoy/ui/theme/envoy_spacing.dart';
+
+class EnvoyLoader extends StatelessWidget {
+  const EnvoyLoader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: EnvoySpacing.medium3,
+      width: EnvoySpacing.medium3,
+      child: CircularProgressIndicator(
+        color: EnvoyColors.tealLight,
+        backgroundColor: EnvoyColors.textPrimaryInverse,
+        strokeWidth: 3,
+      ),
+    );
+  }
+}
