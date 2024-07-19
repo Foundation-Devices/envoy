6218dbdcfd10d9b3333860000307103f0e09c521
Mislav Stipić <mixslav@gmail.com>
2024-07-19 09:48:30 +0200

    BeefQA: Buy bitcoin is grayed if no accounts

---
+++ integration_test/buy_button_test.dart
@@ -0,0 +1,99 @@
+// SPDX-FileCopyrightText: 2024 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import 'package:envoy/main.dart';
+import 'package:envoy/util/console.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:screenshot/screenshot.dart';
+
+void main() {
+  testWidgets('buy button test', (tester) async {
+    final FlutterExceptionHandler? originalOnError = FlutterError.onError;
+    FlutterError.onError = (FlutterErrorDetails details) {
+      kPrint('FlutterError caught: ${details.exceptionAsString()}');
+      if (originalOnError != null) {
+        originalOnError(details);
+      }
+    };
+    try {
+      // Uncomment the line below if testing on local machine.
+      //await resetEnvoyData();
+
+      ScreenshotController envoyScreenshotController = ScreenshotController();
+      await initSingletons();
+      await tester.pumpWidget(Screenshot(
+          controller: envoyScreenshotController, child: const EnvoyApp()));
+
+      // start setup but without accounts
+      await setUpFromStartNoAccounts(tester);
+
+      await findAndPressBuyOptions(tester);
+      await checkBuyOptionAndTitle(tester);
+    } finally {
+      FlutterError.onError = originalOnError;
+    }
+  });
+}
+
+Future<void> findAndPressBuyOptions(WidgetTester tester) async {
+  await tester.pump();
+
+  // Find the GestureDetector containing the QrShield and Buy button text
+  final buyButtonFinder = find.descendant(
+    of: find.byType(GestureDetector),
+    matching: find.text('Buy'),
+  );
+  expect(buyButtonFinder, findsOneWidget);
+
+  // Get the parent GestureDetector widget
+  final gestureDetectorFinder = find.ancestor(
+    of: buyButtonFinder,
+    matching: find.byType(GestureDetector),
+  );
+  expect(gestureDetectorFinder, findsOneWidget);
+
+  // Tap the button
+  await tester.tap(gestureDetectorFinder);
+  await tester.pump(Durations.long2);
+}
+
+Future<void> checkBuyOptionAndTitle(WidgetTester tester) async {
+  await tester.pump();
+
+  // Find the GestureDetector containing the QrShield and Buy button text
+  final buyButtonFinder = find.descendant(
+    of: find.byType(GestureDetector),
+    matching: find.text('Buy'),
+  );
+
+  // Check if the button is still there
+  expect(buyButtonFinder, findsOneWidget);
+
+  // Check if the "ACCOUNTS" title is still there
+  // double check if we entered in BUY
+  final accountsTitleFinder = find.text('ACCOUNTS');
+  expect(accountsTitleFinder, findsOneWidget);
+}
+
+Future<void> setUpFromStartNoAccounts(WidgetTester tester) async {
+  await tester.pump();
+
+  final setUpButtonFinder = find.text('Set Up Envoy Wallet');
+  expect(setUpButtonFinder, findsOneWidget);
+  await tester.tap(setUpButtonFinder);
+  await tester.pump(Durations.long2);
+
+  final continueButtonFinder = find.text('Continue');
+  expect(continueButtonFinder, findsOneWidget);
+  await tester.tap(continueButtonFinder);
+  await tester.pump(Durations.long2);
+
+  // go to home w no accounts
+  final skipButtonFinder = find.text('Skip');
+  expect(skipButtonFinder, findsOneWidget);
+  await tester.tap(skipButtonFinder);
+  await tester.pump(Durations.long2);
+}
