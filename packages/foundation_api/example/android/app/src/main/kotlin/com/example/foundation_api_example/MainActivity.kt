1f1aaf70991f9759a1efa6f07cb93cb6a1b1e3c4
Igor Cota <igor@openbook.hr>
2024-07-21 14:16:58 +0200

    packages: add foundation_api

---
+++ packages/foundation_api/example/android/app/src/main/kotlin/com/example/foundation_api_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foundation_api_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ packages/foundation_api/lib/foundation_api.dart
@@ -0,0 +1,4 @@
+library foundation_api;
+
+export 'src/rust/api/simple.dart';
+export 'src/rust/frb_generated.dart' show RustLib;
