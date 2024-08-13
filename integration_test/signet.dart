1b6be773bfc525881c0af81377b695d829e61ee9
Darijo Perković <darijo@openbook.hr>
2024-08-13 13:52:00 +0200

    Remove continue-on-error

--- integration_test/signet.dart
+++ integration_test/signet.dart
@@ -31,7 +31,7 @@ void main() {
       await tester.pumpWidget(Screenshot(
           controller: envoyScreenshotController, child: const EnvoyApp()));
 
-      await setUpAppFromStart(tester);
+      // await setUpAppFromStart(tester);
 
       await fromHomeToAdvancedMenu(tester);
 
