81dc4a8d3b58054b7da8a178bb066c01e0086d52
Darijo Perković <darijo@openbook.hr>
2024-08-20 12:13:12 +0200

    fail GH action when test fail

--- run_integration_tests.sh
+++ run_integration_tests.sh
@@ -4,6 +4,9 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+# Initialize exit code
+exit_code=0
+
 # Iterate over each test file in integration_test_order
 while IFS= read -r test_file; do
     # Skip lines that start with #
@@ -11,6 +14,11 @@ while IFS= read -r test_file; do
         continue
     fi
 
-    # Run the test for the current file
-    xvfb-run -a -s '-screen 0 1024x768x24 +extension GLX' flutter test "$test_file" -d linux --verbose
-done < integration_test_order
+  # Run the test for the current file
+  if ! xvfb-run -a -s '-screen 0 1024x768x24 +extension GLX' flutter test "$test_file" -d linux --verbose; then
+    exit_code=1
+  fi
+done <integration_test_order
+
+# Exit with the appropriate code
+exit $exit_code
