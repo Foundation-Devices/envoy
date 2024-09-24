05f317ff6d45c4463cf89eb421f188c314652c68
Igor Cota <igor@openbook.hr>
2024-09-24 14:34:36 +0200

    Add licences

--- test/tx_sorting_test.dart
+++ test/tx_sorting_test.dart
@@ -1,6 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 import 'dart:convert';
 import 'dart:io';
-import 'dart:math';
 
 import 'package:envoy/util/console.dart';
 import 'package:flutter_test/flutter_test.dart';
