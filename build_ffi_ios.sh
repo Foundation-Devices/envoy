3582c4bc469a61165815ce74eaded45de7f9d592
Igor Cota <igor@openbook.hr>
2024-12-19 19:55:19 +0100

    Fix Rust build

--- build_ffi_ios.sh
+++ build_ffi_ios.sh
@@ -8,15 +8,14 @@
 set -e
 
 # iOS
-rustup install 1.69.0
-rustup +1.69.0 target add aarch64-apple-ios
-cargo +1.69.0 build --target=aarch64-apple-ios
-cargo +1.69.0 build --target=aarch64-apple-ios --release
+rustup target add aarch64-apple-ios
+cargo build --target=aarch64-apple-ios
+cargo build --target=aarch64-apple-ios --release
 
 # iOS simulator
 # (This'll work when https://github.com/alexcrichton/openssl-src-rs/pull/131 is merged/released)
-rustup +1.69.0 target add aarch64-apple-ios-sim
-cargo +1.69.0 build --target=aarch64-apple-ios-sim
+rustup target add aarch64-apple-ios-sim
+cargo build --target=aarch64-apple-ios-sim
 
 # Workaround to not being able to specify --crate-type=staticlib on a virtual package
 # Having the dynamic libraries in these folders results in a runtime error
