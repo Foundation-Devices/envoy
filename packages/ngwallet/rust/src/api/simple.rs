7a7036cf019c769a3ee814220a20c9f616f8b6ff
Igor Cota <igor@openbook.hr>
2025-03-10 17:45:21 +0100

    Add ngwallet

---
+++ packages/ngwallet/rust/src/api/simple.rs
@@ -0,0 +1,16 @@
+use ngwallet::NgWallet;
+
+#[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
+pub fn greet(name: String) -> String {
+    format!("Hello, {name}!")
+}
+
+#[flutter_rust_bridge::frb(init)]
+pub fn init_app() {
+    // Default utilities - feel free to customize
+    flutter_rust_bridge::setup_default_user_utils();
+}
+
+pub fn get_wallet() -> NgWallet {
+    NgWallet::new().unwrap_or(NgWallet::load().unwrap())
+}
