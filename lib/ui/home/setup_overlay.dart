10751ed7bf53079ff5f027630b0652f10d7551db
Mislav <mixslav@gmail.com>
2025-04-01 08:53:42 +0200

    Add drag, refining card and button

---
+++ lib/ui/home/setup_overlay.dart
@@ -0,0 +1,333 @@
+// SPDX-FileCopyrightText: 2023 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import 'package:envoy/business/account_manager.dart';
+import 'package:envoy/ui/theme/envoy_spacing.dart';
+import 'package:envoy/ui/widgets/color_util.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:envoy/generated/l10n.dart';
+import 'package:envoy/ui/components/stripe_painter.dart';
+import 'package:envoy/ui/glow.dart';
+import 'package:envoy/ui/theme/envoy_colors.dart';
+import 'package:envoy/ui/theme/envoy_typography.dart';
+
+double cardButtonHeight = 125;
+
+class AnimatedBottomOverlay extends ConsumerStatefulWidget {
+  const AnimatedBottomOverlay({super.key});
+
+  @override
+  ConsumerState createState() => _AnimatedBottomOverlayState();
+}
+
+class _AnimatedBottomOverlayState extends ConsumerState<AnimatedBottomOverlay>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  final double overlayHeight = (cardButtonHeight + EnvoySpacing.medium3) *
+          (AccountManager().hotAccountsExist() ? 2 : 1) +
+      EnvoySpacing.large2 * 2; // + extra
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+    _animation = Tween<double>(begin: 0, end: overlayHeight).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeOut),
+    );
+
+    _controller.forward(); // Start animation
+  }
+
+  void _closeOverlay() {
+    _controller.reverse().then((_) async {
+      if (mounted) {
+        Navigator.of(context).pop();
+
+        await Future.delayed(const Duration(milliseconds: 100));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _closeOverlay,
+      behavior: HitTestBehavior.opaque, // Close when tapping outside
+      child: AnimatedBuilder(
+        animation: _animation,
+        builder: (context, child) {
+          return Align(
+            alignment: Alignment.bottomCenter,
+            child: GestureDetector(
+              onTap: () {}, // Prevent taps inside the overlay
+              onVerticalDragUpdate: (details) {
+                _controller.value -= details.primaryDelta! / overlayHeight;
+              },
+              onVerticalDragEnd: (details) {
+                if (_controller.value < 0.5) {
+                  _closeOverlay(); // Close if dragged down enough
+                } else {
+                  _controller.forward(); // Snap back if not enough
+                }
+              },
+
+              child: Transform.scale(
+                scale: 1.0,
+                child: SizedBox(
+                  height: _animation.value,
+                  width: MediaQuery.of(context).size.width,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black.applyOpacity(0.2),
+                          spreadRadius: 0,
+                          blurRadius: 10,
+                          offset: const Offset(0, 0),
+                        ),
+                      ],
+                    ),
+                    child: Card(
+                      elevation: 100,
+                      shadowColor: Colors.black,
+                      shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(EnvoySpacing.medium2),
+                        ),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Container(
+                              width: 40,
+                              height: 4,
+                              margin: const EdgeInsets.only(
+                                  top: EnvoySpacing.xs,
+                                  bottom: EnvoySpacing.small),
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(2),
+                              )),
+                          Expanded(
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: EnvoySpacing.medium1),
+                              child: SingleChildScrollView(
+                                physics: const NeverScrollableScrollPhysics(),
+                                // this is to remove overflow warning while animating
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.max,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    if (AccountManager().hotAccountsExist())
+                                      Column(
+                                        children: [
+                                          const SizedBox(
+                                              height: EnvoySpacing.medium3),
+                                          EnvoyCardButton(
+                                            image:
+                                                'assets/welcome_envoy_sm.png',
+                                            title: S()
+                                                .onboarding_welcome_createMobileWallet,
+                                            onTap: () {
+                                              // TODO
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    const SizedBox(
+                                        height: EnvoySpacing.medium3),
+                                    EnvoyCardButton(
+                                      image: 'assets/passport_and_prime.png',
+                                      imagePaddingLeft: 12,
+                                      title:
+                                          S().onboarding_welcome_setUpPassport,
+                                      onTap: () {
+                                        // TODO
+                                      },
+                                    ),
+                                    const SizedBox(
+                                        height: EnvoySpacing.medium3),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class EnvoyCardButton extends StatelessWidget {
+  final String image;
+  final String title;
+  final double imagePaddingLeft;
+  final VoidCallback onTap;
+
+  const EnvoyCardButton({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.onTap,
+    this.imagePaddingLeft = 0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double cardButtonWidth = MediaQuery.of(context).size.width * 0.9;
+    const cardRadius = EnvoySpacing.medium2;
+
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: cardButtonWidth,
+        height: cardButtonHeight,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(cardRadius),
+          boxShadow: const [
+            BoxShadow(
+              color: EnvoyColors.textInactive,
+              blurRadius: 6,
+              spreadRadius: 2,
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(cardRadius),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(cardRadius),
+                            bottomLeft: Radius.circular(cardRadius),
+                          ),
+                          child: Container(
+                            color: EnvoyColors.textPrimaryInverse,
+                          ),
+                        ),
+                        Glow(
+                          innerColor:
+                              EnvoyColors.accentSecondary.applyOpacity(0.1),
+                          middleColor:
+                              EnvoyColors.accentSecondary.applyOpacity(0.1),
+                          outerColor:
+                              EnvoyColors.textPrimaryInverse.applyOpacity(0.85),
+                          stops: const [0.0, 0.05, 1.0],
+                        ),
+                        Positioned.fill(
+                          child: ShaderMask(
+                            shaderCallback: (Rect bounds) {
+                              return const LinearGradient(
+                                begin: Alignment.topCenter,
+                                end: Alignment.bottomCenter,
+                                colors: [
+                                  Colors.transparent,
+                                  Colors.transparent,
+                                  Colors.white,
+                                ],
+                                stops: [0.0, 0.3, 1.0],
+                              ).createShader(bounds);
+                            },
+                            blendMode: BlendMode.dstIn,
+                            child: CustomPaint(
+                              isComplex: true,
+                              willChange: false,
+                              painter: StripePainter(
+                                EnvoyColors.gray1000.applyOpacity(0.1),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 1,
+                    color: EnvoyColors.accentSecondary,
+                    height: double.infinity,
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: ClipRRect(
+                      borderRadius: const BorderRadius.only(
+                        topRight: Radius.circular(cardRadius),
+                        bottomRight: Radius.circular(cardRadius),
+                      ),
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              EnvoyColors.textSecondary,
+                              EnvoyColors.textPrimary,
+                            ],
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.only(left: EnvoySpacing.medium1),
+                          child: Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              title,
+                              textAlign: TextAlign.start,
+                              style: EnvoyTypography.heading.copyWith(
+                                color: EnvoyColors.textPrimaryInverse,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Positioned(
+                left: imagePaddingLeft,
+                top: 12,
+                child: Image.asset(
+                  image,
+                  width: 110,
+                  height: 110,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
