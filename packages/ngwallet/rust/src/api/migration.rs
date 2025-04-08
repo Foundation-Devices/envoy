e853a71cb5120a42030cc8644e789b60e7de0b86
Igor Cota <igor@openbook.hr>
2025-04-08 14:52:10 +0200

    Fix licencing and ngwallet build

--- packages/ngwallet/rust/src/api/migration.rs
+++ packages/ngwallet/rust/src/api/migration.rs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 use std::collections::HashMap;
 use std::error::Error;
 use std::ops::Index;
