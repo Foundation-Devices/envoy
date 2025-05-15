2ed533e22fdbfb94495482bce11afe3dd23b0b46
dariyoo99 <darijo@openbook.hr>
2025-05-15 15:27:37 +0200

    turn of Tor and false secure checked

--- lib/business/settings.dart
+++ lib/business/settings.dart
@@ -206,7 +206,7 @@ class Settings extends ChangeNotifier {
     return !usingDefaultElectrumServer;
   }
 
-  bool usingTor = true;
+  bool usingTor = false;
 
   bool torEnabled() {
     return usingTor;
