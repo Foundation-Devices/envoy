dde8e09071a5d0d624c826a517072cf4cbca51e1
Igor Cota <igor@openbook.hr>
2025-05-20 18:18:08 +0200

    bluart: reduce duration between writes to 10ms

--- packages/bluart/rust/src/api/ble/peripheral.rs
+++ packages/bluart/rust/src/api/ble/peripheral.rs
@@ -87,7 +87,7 @@ impl Device {
         for data in data {
             self.peripheral.write(&uart_characteristic, &data, WriteType::WithoutResponse)
                 .await?;
-            tokio::time::sleep(time::Duration::from_millis(60)).await;
+            tokio::time::sleep(time::Duration::from_millis(10)).await;
         }
 
         let duration = start.elapsed();
