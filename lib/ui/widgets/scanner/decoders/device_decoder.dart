97e6bc189f85c2fa541bcca0e56b0367ad3c9d59
invertedx <invertedx@foundationdevices.com>
2025-05-23 20:03:37 +0200

    fix home screen scanner for passport paring

---
+++ lib/ui/widgets/scanner/decoders/device_decoder.dart
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2025 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+import 'package:envoy/ui/widgets/scanner/decoders/pair_decoder.dart';
+import 'package:envoy/ui/widgets/scanner/scanner_decoder.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class DeviceDecoder extends ScannerDecoder {
+  final Function(String code) onScan;
+  PairPayloadDecoder pairPayloadDecoder;
+
+  DeviceDecoder( {required this.onScan,required this.pairPayloadDecoder});
+
+  @override
+  Future<void> onDetectBarCode(Barcode barCode) async {
+    if (barCode.code == null) {
+      return;
+    }
+    if (barCode.code?.toLowerCase().startsWith("ur:") == true) {
+      pairPayloadDecoder.onDetectBarCode(barCode);
+      progressCallBack?.call(pairPayloadDecoder.urDecoder.urDecoder.progress);
+    }else{
+      onScan(barCode.code!);
+    }
+  }
+}
