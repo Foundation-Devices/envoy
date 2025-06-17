82f0fad5fd0f614e6b421fb2460d6818bddf1e6d
Mislav <mixslav@gmail.com>
2025-06-17 09:10:26 +0200

    Fix staging pop-up

--- lib/ui/home/cards/accounts/spend/state/spend_state.dart
+++ lib/ui/home/cards/accounts/spend/state/spend_state.dart
@@ -45,7 +45,7 @@ final draftTransactionProvider = Provider<DraftTransaction?>((ref) {
   return ref.watch(spendTransactionProvider).draftTransaction;
 });
 
-// If the user performed coin selection or finalized a non-hot wallet transaction,
+// If the user performed coin selection or finalized a wallet transaction,
 // they should see a confirmation dialog before exiting the review screen.
 final isTransactionCancellableProvider = Provider<bool>((ref) {
   EnvoyAccount? account = ref.watch(selectedAccountProvider);
@@ -56,11 +56,9 @@ final isTransactionCancellableProvider = Provider<bool>((ref) {
   bool userChangedCoins = ref.watch(userSelectedCoinsThisSessionProvider);
   final finalizedTx = transactionModel.draftTransaction?.isFinalized ?? false;
   bool cancellable = true;
-  if (account.isHot) {
-    cancellable = !userChangedCoins;
-  } else {
-    cancellable = !finalizedTx && !userChangedCoins;
-  }
+
+  cancellable = !finalizedTx && !userChangedCoins;
+
   return cancellable;
 });
 
