a8d9d38fd58873b64e918968a79560a8949c7c14
dariyoo99 <darijo@openbook.hr>
2025-06-27 11:51:39 +0200

    fix flutter analyzer

--- lib/ui/onboard/onboard_welcome_passport.dart
+++ lib/ui/onboard/onboard_welcome_passport.dart
@@ -2,13 +2,10 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-import 'package:envoy/account/accounts_manager.dart';
-import 'package:envoy/business/local_storage.dart';
 import 'package:envoy/generated/l10n.dart';
 import 'package:envoy/ui/envoy_button.dart';
 import 'package:envoy/ui/onboard/onboarding_page.dart';
 import 'package:envoy/ui/onboard/routes/onboard_routes.dart';
-import 'package:envoy/ui/routes/routes.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
@@ -36,8 +33,6 @@ class _OnboardPassportWelcomeScreenState
 
   @override
   Widget build(BuildContext context) {
-    final bool onboardingComplete =
-        LocalStorage().prefs.getBool(PREFS_ONBOARDED) ?? false;
     return EnvoyPatternScaffold(
       // shield: Container(
       //   margin: const EdgeInsets.all(EnvoySpacing.medium1),
