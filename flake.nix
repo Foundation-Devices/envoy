4b1e162e6f36a314ffd68879abb9ff5c06f877a6
Igor Cota <igor@openbook.hr>
2025-10-31 13:11:50 +0100

    feat: Use rust-overlay and rust-toolchain
    This commit updates the flake.nix to use rust-overlay and rust-toolchain, providing a more streamlined and reproducible Rust development environment.

--- flake.nix
+++ flake.nix
@@ -7,17 +7,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
+    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = import nixpkgs {
           inherit system;
+          overlays = [(import rust-overlay)];
           config = {
             allowUnfree = true;
             android_sdk.accept_license = true;
@@ -56,15 +59,12 @@
             (xcodeenv.composeXcodeWrapper {versions = ["16.0"];})
           ];
 
+        rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
         buildInputs = with pkgs;
           [
             # Rust tools
-            rustup
-            rustc
-            cargo
-            rustfmt
-            clippy
-            rust-analyzer
+            rustToolchain
             rust-bindgen
 
             # Flutter
