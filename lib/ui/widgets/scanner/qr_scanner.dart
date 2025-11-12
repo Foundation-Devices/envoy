ecc74f4b59a9d7e0f013459cfb5ad6325a579379
Mislav <mixslav@gmail.com>
2025-11-12 10:55:36 +0100

    [2.3] Add modal on SCV scan

--- lib/ui/widgets/scanner/qr_scanner.dart
+++ lib/ui/widgets/scanner/qr_scanner.dart
@@ -5,11 +5,9 @@
 import 'dart:async';
 import 'dart:io';
 
-import 'package:envoy/generated/l10n.dart';
 import 'package:envoy/ui/envoy_colors.dart';
 import 'package:envoy/ui/widgets/blur_dialog.dart';
 import 'package:envoy/ui/widgets/scanner/scanner_decoder.dart';
-import 'package:envoy/ui/widgets/toast/envoy_toast.dart';
 import 'package:envoy/util/rive_util.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -222,15 +220,34 @@ class _QrScannerState extends State<QrScanner>
 
       try {
         await widget.decoder.onDetectBarCode(barcode);
+      } on UnableToDecodeException catch (e) {
+        if (context.mounted) {
+          widget.decoder.onDecodeError(
+            context,
+            e,
+            onRetry: () {
+              Navigator.pop(context); // Close popup first
+              setState(() {
+                _lastScan = "";
+                _progress = 0;
+                _lastCodeDetected = "";
+                _lastRawBytesDetected = [];
+              });
+              controller.resumeCamera();
+            },
+            onCancel: () {
+              Navigator.pop(context); // Close popup
+              Navigator.pop(context); // Close camera
+            },
+          );
+          controller.pauseCamera();
+        }
       } catch (e) {
         if (context.mounted) {
-          EnvoyToast(
-            replaceExisting: true,
-            message: e.toString(),
-            actionButtonText: S().component_retry,
-            isDismissible: false,
-            onActionTap: () {
-              EnvoyToast.dismissPreviousToasts(context);
+          widget.decoder.onDecodeError(
+            context,
+            e as Exception,
+            onRetry: () {
               setState(() {
                 _lastScan = "";
                 _progress = 0;
@@ -238,11 +255,7 @@ class _QrScannerState extends State<QrScanner>
                 _lastRawBytesDetected = [];
               });
             },
-            icon: const Icon(
-              Icons.info_outline,
-              color: EnvoyColors.white95,
-            ),
-          ).show(context);
+          );
         }
       } finally {
         _lastScan = barcode.code ?? '';
