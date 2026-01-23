7f72b7cef471a0a45c7aca2a422172f234f4cc5a
Mislav <mixslav@gmail.com>
2026-01-23 11:11:54 +0100

    Add licence information

--- scripts/run_android_maestro.sh
+++ scripts/run_android_maestro.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: 2026 Foundation Devices Inc.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
 # Maestro Android Test Runner for Envoy
 # Usage: ./run_android_maestro.sh [test_name.yaml]
 
