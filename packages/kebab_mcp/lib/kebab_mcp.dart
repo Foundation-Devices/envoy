435f23a2155b23c48052ecb30bd0ea4996c019e8
Mislav <mixslav@gmail.com>
2026-02-05 13:56:47 +0100

    Kebab mcp

---
+++ packages/kebab_mcp/lib/kebab_mcp.dart
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Foundation Devices Inc.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+library kebab_mcp;
+
+export 'src/kebab_server.dart';
+export 'src/mqtt_client.dart';
